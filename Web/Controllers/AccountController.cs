e47b44ff021a2fc34cc5d41d9cb09934584a1100
ozan.kaplan <ozan.kaplan@outlook.com>
2021-11-05 19:23:47 +0300

    Custom css dosyası eklendi. Login ve Register View'leri Eklendi.

--- Web/Controllers/AccountController.cs
+++ Web/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Web.Models;
 
 namespace Web.Controllers
 {
@@ -12,6 +13,20 @@ namespace Web.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Login()
+        {
+            UserViewModel userViewModel = new UserViewModel()
+            {
+                Id = 1,
+                Name = "Ozan",
+                Surname = "Kaplan",
+                Username = "admin"
+
+            };
+
+            return View(userViewModel);
         }
     }
 }
